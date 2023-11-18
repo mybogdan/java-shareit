2eeedc1fb7c54d330f3e4f847e1351cc4ea0b466
mybogdan <bogdan.vaseev@gmail.com>
2023-11-18 21:45:46 +0300

    add-controllers

--- src/main/java/ru/practicum/shareit/user/service/UserService.java
+++ src/main/java/ru/practicum/shareit/user/service/UserService.java
@@ -16,6 +16,7 @@ import java.util.List;
 public class UserService {
 
     private final InMemoryUserStorageImpl userStorage;
+
     public List<UserDto> getAllUsers() {
         return UserMapper.toUsersDto(userStorage.getAllUsers());
     }
